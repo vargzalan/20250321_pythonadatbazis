3d6887c10fa0250fbbd65a5241b35edb9f944e15
vargzalan <varga.zalan@diak.szbi-pg.hu>
2025-03-21 13:30:40 +0100

    Update kiralyok.py

--- kiralyok.py
+++ kiralyok.py
@@ -4,15 +4,30 @@ cnx = connection.MySQLConnection(user='root', password='mysql',
                                  host='127.0.0.1',
                                  database='kiralyok')
 
-
+#táblák megjelenítése
 cursor = cnx.cursor()
 cursor.execute("SHOW TABLES")
 for table in cursor:
     print(table)
+print("-----")
 
+#uralkodó megjelenítése
+cursor.execute("SELECT * FROM uralkodo")
+for uralkodo in cursor:
+    print(uralkodo)
+print("-----")
 
+#matyas kiraly
+cursor.execute("SELECT * FROM uralkodo WHERE uralkodo.nev='I. Mátyás'")
+for uralkodo in cursor:
+    print(uralkodo)   
+print("-----")
 
-
+#matlyas kiral adatai
+cursor.execute("SELECT szul,hal FROM uralkodo WHERE uralkodo.nev='I. Mátyás'")
+for uralkodo in cursor:
+    print(f"Mályás király élt:{uralkodo}")   
+print("-----")
 
 
 
